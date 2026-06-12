52010e7675047d2630802db045f36c726b8afb10
Xavier Andrade <xaandrade@uce.edu.ec>
2026-06-12 16:45:20 -0500

    ci/cd

--- notification-service/test_main.py
+++ notification-service/test_main.py
@@ -1,15 +1,26 @@
-import sys
-from unittest.mock import MagicMock
-
-sys.modules['confluent_kafka'] = MagicMock()
-sys.modules['kafka'] = MagicMock()
-
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
-def test_read_main_frontend_notification():
+def test_home():
     response = client.get("/")
+
     assert response.status_code == 200
     assert "Notification Microservice" in response.text
+
+def test_enviar_notificacion():
+    payload = {
+        "estudiante_id": 123,
+        "mensaje": "Prueba QA",
+        "canal": "email"
+    }
+
+    response = client.post("/enviar", json=payload)
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["despachado"] is True
+    assert data["canal_utilizado"] == "EMAIL"
